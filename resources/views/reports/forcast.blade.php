fef89ba4bb9aa63c3bae4ebeeba8ef47a8fbd0db
Iwan Sutandi <sutandi@student.spb.ac.id>
2022-05-14 09:03:15 +0800

    v1

---
+++ resources/views/reports/forcast.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Hasil Ramalan Klaim BPJS </title>
+
+
+    <style type="text/css">
+        img {
+            width: 40px;
+            height: 40px;
+            border-radius: 100%;
+        }
+
+        .center {
+            text-align: center;
+        }
+
+    </style>
+
+</head>
+
+<body>
+
+    <h2 style="text-transform: uppercase;">Laporan Hasil Ramalan Klaim BPJS {{ $forcast->tahun_ramalan }}</h2>
+
+
+
+</body>
+
+</html>
